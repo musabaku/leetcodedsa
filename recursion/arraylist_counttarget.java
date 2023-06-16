4d7798d8ad784daecf73ff5d793e29c10e2191ab
MUSAB AHMED KHAN UMAIR <110651555+musabaku@users.noreply.github.com>
2023-06-16 10:16:25 +0300

    Update arraylist_counttarget.java

--- recursion/arraylist_counttarget.java
+++ recursion/arraylist_counttarget.java
@@ -16,3 +16,24 @@ class HelloWorld {
     return count(arr,t,i+1,list);
     }
 }
+
+// return type void
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,2,3,4,2,5,6};
+        ArrayList<Integer> list = new ArrayList<>();
+        count(arr,2,0,list);
+        System.out.println(list);
+    }
+    static void count(int[] arr,int t,int i,ArrayList<Integer> list){
+        
+    if(i==arr.length){
+        return;
+    }
+    if(t == arr[i]){
+        list.add(i);
+    }
+    count(arr,t,i+1,list);
+    }
+}
