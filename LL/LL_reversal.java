bd864f17e28537cb6f5050ab48fad4120a35889e
Musab Ahmed Khan Umair <110651555+musabaku@users.noreply.github.com>
2023-12-23 07:00:41 +0300

    Add files via upload

---
+++ LL/LL_reversal.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = head.next;
+
+    while(current!=null){
+        current.next=prev;
+        prev = current;
+        current = next;
+        if(next!=null){
+        next=next.next;
+        }
+
+    }
+    return prev;
+    }
+}
+
+// https://leetcode.com/problems/reverse-linked-list/submissions/
