5fb53a1b9255c450e9f862964ee805d65f202be2
Musab Ahmed Khan Umair <110651555+musabaku@users.noreply.github.com>
2025-01-09 10:22:46 +0300

    Create isSubtree.java

---
+++ trees/2025_Trees/isSubtree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   
+   
+    public boolean helper(TreeNode root, TreeNode subRoot){
+        if(subRoot==null&&root==null){
+            return true;
+        }
+           if(subRoot==null||root==null){
+            return false;
+        }
+        
+        return root.val==subRoot.val && helper(root.left,subRoot.left)&& helper(root.right,subRoot.right);
+
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+         if(subRoot==null&&root==null){
+            return true;
+        }
+           if(subRoot==null||root==null){
+            return false;
+        }
+       if(helper(root,subRoot)) return true;
+        return helper(root.left,subRoot) || helper(root.right,subRoot);
+    }
+}
