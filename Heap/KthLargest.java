affdae6962d17ae09a7affe9542d7c246dc023cd
Musab Ahmed Khan Umair <110651555+musabaku@users.noreply.github.com>
2025-01-17 12:24:44 +0300

    Create KthLargest.java

---
+++ Heap/KthLargest.java
@@ -0,0 +1,25 @@
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    int k=0;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for(int num:nums){
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if(pq.size()>k){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
